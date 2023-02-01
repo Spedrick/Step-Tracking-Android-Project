f988bbef3941658d425cfa2a4377428f83d467e8
jogesh <jogeshsoni6@gmail.com>
2023-02-01 17:27:18 +0530

    added a toast

--- app/src/main/java/com/example/steptracking/MainActivity.kt
+++ app/src/main/java/com/example/steptracking/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.steptracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this,"Clicked",Toast.LENGTH_LONG).show()
+        }
     }
 }
