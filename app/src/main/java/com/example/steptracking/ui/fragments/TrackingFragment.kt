236aa08eed49a01e54c9dbc08b0f90e3921c7a4a
Spedrick <img_2021066@iiitm.ac.in>
2023-02-16 15:37:22 +0530

    Implemented saving username and weight to the database Setup fragment appears only when the app is launched for the first time

--- app/src/main/java/com/example/steptracking/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/steptracking/ui/fragments/TrackingFragment.kt
@@ -28,6 +28,7 @@ import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 import java.util.*
+import javax.inject.Inject
 import kotlin.math.round
 
 @AndroidEntryPoint
@@ -43,7 +44,8 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking){
 
     private var menu: Menu? = null
 
-    private var weight = 80f
+    @set:Inject
+    var weight = 80f
 
     override fun onCreateView(
         inflater: LayoutInflater,
